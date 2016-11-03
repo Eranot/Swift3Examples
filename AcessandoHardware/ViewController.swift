80a520681a33bd6e4540164259d5d12603415f99
suporte suporte <suporte@unochapeco.edu.br>
2016-11-03 13:35:30 -0200

    Initial Commit

---
+++ AcessandoHardware/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AcessandoHardware
+//
+//  Created by Unochapeco unochapeco on 03/11/16.
+//  Copyright © 2016 Unochapeco unochapeco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
